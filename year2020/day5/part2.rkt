f3b06e815aa291f42a0bbcaf8fa7dc2dc3eb49c2
Jack Firth <jackhfirth@gmail.com>
2020-12-07 00:28:53 -0800

    Solve day 5 part 2

---
+++ year2020/day5/part2.rkt
@@ -0,0 +1,66 @@
+#lang racket/base
+
+(require advent-of-code/private/regexp-parsing
+         racket/file
+         racket/match
+         racket/runtime-path
+         racket/set
+         racket/string
+         rebellion/collection/entry
+         rebellion/collection/hash
+         rebellion/collection/vector
+         rebellion/private/guarded-block
+         rebellion/streaming/reducer
+         rebellion/streaming/transducer)
+
+(module+ test
+  (require rackunit
+           rebellion/private/static-name))
+
+;@--------------------------------------------------------------------------------------------------
+
+(define-runtime-path input "input.txt")
+
+(define (row-id row-code)
+  (for/fold ([row 0]) ([code-char (in-string row-code)])
+      (match code-char [#\F (* row 2)] [#\B (add1 (* row 2))])))
+
+(define (column-id column-code)
+  (for/fold ([column 0]) ([code-char (in-string column-code)])
+      (match code-char [#\L (* column 2)] [#\R (add1 (* column 2))])))
+
+(define (seat-id boarding-pass)
+  (regexp-define (row-code column-code) #px"([FB]{7})([LR]{3})" boarding-pass)
+  (define row (row-id row-code))
+  (define column (column-id column-code))
+  (+ (* row 8) column))
+
+(module+ test
+  (test-case (name-string row-id)
+    (check-equal? (row-id "FBFBBFF") 44)
+    (check-equal? (row-id "BFFFBBF") 70)
+    (check-equal? (row-id "FFFBBBF") 14)
+    (check-equal? (row-id "BBFFBBF") 102))
+
+  (test-case (name-string column-id)
+    (check-equal? (column-id "RLR") 5)
+    (check-equal? (column-id "RRR") 7)
+    (check-equal? (column-id "RLL") 4))
+
+  (test-case (name-string seat-id)
+    (check-equal? (seat-id "FBFBBFFRLR") 357)
+    (check-equal? (seat-id "BFFFBBFRRR") 567)
+    (check-equal? (seat-id "FFFBBBFRRR") 119)
+    (check-equal? (seat-id "BBFFBBFRLL") 820)))
+
+(define seats
+  (transduce (file->lines input)
+             (mapping seat-id)
+             (sorting)
+             #:into (into-vector)))
+
+(for/or ([i (in-naturals)] [j (in-range 1 (vector-length seats))])
+  (define seat-i (vector-ref seats i))
+  (define seat-j (vector-ref seats j))
+  (and (not (equal? seat-i (sub1 seat-j)))
+       (add1 seat-i)))
